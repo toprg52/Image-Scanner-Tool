64a88cf1f648d3f626d75b060c6205d3288e883b
Ryan Gupta <ryan.mitblr2024@learner.manipal.edu>
2026-06-27 17:18:54 +0530

    Add --output arg to scan.py, process img16-21

--- scan.py
+++ scan.py
@@ -252,10 +252,10 @@ class DocScanner(object):
 
 
 
-    def scan(self, image_path):
+    def scan(self, image_path, output_dir='output'):
 
         RESCALED_HEIGHT = 500.0
-        OUTPUT_DIR = 'output'
+        OUTPUT_DIR = output_dir
 
         # load the image and compute the ratio of the old height
         # to the new height, clone it, and resize it
@@ -287,9 +287,11 @@ if __name__ == "__main__":
     group = ap.add_mutually_exclusive_group(required=True)
     group.add_argument("--images", help="Directory of images to be scanned")
     group.add_argument("--image", help="Path to single image to be scanned")
+    ap.add_argument("--output", help="Output directory", default="output")
     args = vars(ap.parse_args())
     im_dir = args["images"]
     im_file_path = args["image"]
+    output_dir = args["output"]
 
     scanner = DocScanner()
 
@@ -299,10 +301,10 @@ if __name__ == "__main__":
 
     # Scan single image specified by command line argument --image <IMAGE_PATH>
     if im_file_path:
-        scanner.scan(im_file_path)
+        scanner.scan(im_file_path, output_dir)
 
     # Scan all valid images in directory specified by command line argument --images <IMAGE_DIR>
     else:
         im_files = [f for f in os.listdir(im_dir) if get_ext(f) in valid_formats]
         for im in im_files:
-            scanner.scan(im_dir + '/' + im)
+            scanner.scan(im_dir + '/' + im, output_dir)
